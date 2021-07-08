f5fc714967abd034f690d68029dc3112b69cfcec
lucianom <lucianom@xmos.com>
2021-07-08 12:42:33 +0100

    Add support for QF60 board

--- tests/test_mic_input/src/mic_array_conf.h
+++ tests/test_mic_input/src/mic_array_conf.h
@@ -5,5 +5,15 @@
 #define MIC_ARRAY_MAX_FRAME_SIZE_LOG2 7
 #define MIC_ARRAY_NUM_MICS 8
 
-
+#if (SQ_MIC_ARRAY == 1)
+#define MIC_ARRAY_CH0      PIN4
+#define MIC_ARRAY_CH1      PIN7
+#define MIC_ARRAY_CH2      PIN6
+#define MIC_ARRAY_CH3      PIN5
+#else
+#define MIC_ARRAY_CH0      PIN4
+#define MIC_ARRAY_CH1      PIN7
+#define MIC_ARRAY_CH2      PIN5
+#define MIC_ARRAY_CH3      PIN6
+#endif
 #endif /* MIC_ARRAY_CONF_H_ */
